2e2ae8fcc24da320d8b21754859e855e7be2ca53
Jokin <327928971@qq.com>
2019-04-20 16:52:34 +0800

    add captcha

--- config/app.php
+++ config/app.php
@@ -161,6 +161,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        // 验证码
+        Mews\Captcha\CaptchaServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -225,6 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        // 验证码
+        'Captcha' => Mews\Captcha\Facades\Captcha::class,
 
     ],
 
