ac163281ce25251071ce148f0cbcdd31a173c10a
jokin <327928971@qq.com>
2019-07-13 23:11:29 +0800

    optimize user_resources view

--- resources/views/user/user_resources.blade.php
+++ resources/views/user/user_resources.blade.php
@@ -17,7 +17,7 @@
     <tr>
       <td><img class="lazy" src="{{ asset('img/loading.svg') }}" data-src="{{ $_system['cdn_prefix'] }}{{ $item->image }}" alt="{{ $item->iname }}" height="18x;"></td>
       <td>{{ $item->iname }}</td>
-      <td class="text-truncate">{{ $item->description }}</td>
+      <td class="text-truncate" title="{{ $item->description }}">{{ $item->description }}</td>
       <td>{{ isset($user_items[$item->iid]['count']) ? $user_items[$item->iid]['count'] : 0 }}</td>
     </tr>
     @endforeach
