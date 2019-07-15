0eaca90e31667d99887214806e897f0352ca581d
jokin <327928971@qq.com>
2019-07-15 10:30:05 +0800

    fix recycle view bug

--- resources/views/user/recycle_center.blade.php
+++ resources/views/user/recycle_center.blade.php
@@ -47,7 +47,7 @@
             <span class="input-group-text">回收数量</span>
           </div>
           <input type="hidden" class="form-control" placeholder="回收物品" id="recycle_iid" value="1">
-          <input type="number" class="form-control" placeholder="回收数量" id="recycle_count" value="1" min="1" onchange="calc();" onkeydown="calc();">
+          <input type="number" class="form-control" placeholder="回收数量" id="recycle_count" value="1" min="1" onchange="calc();" onkeyup="calc();">
         </div>
         <p>
           本次回收预计到账 <span id="anticipate_point">0</span> 积分
